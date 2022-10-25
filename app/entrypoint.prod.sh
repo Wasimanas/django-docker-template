5725d08219c2ee89eef8db00b7518a03b80d3b8f
BiomedLearning <wasimanas12@gmail.com>
2022-10-25 10:39:53 +0530

    First Commit

---
+++ app/entrypoint.prod.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput  
+python manage.py create_admin_user
+
+
+exec "$@"

1d5737d713509bbcb60ec5876e99611581605e31
Wasim Mohammed <wasimanas12@gmail.com>
2023-04-20 06:25:59 +0530

    django-docker-template

--- app/quiz/routing.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path 
-from quiz import consumers 
-
-
-websocket_urlpatterns = [
-        path('ws/quiz/<slug:quizCategorySlug>/',consumers.QuizConsumer.as_asgi(),name='quiz'),
-        ]
-
